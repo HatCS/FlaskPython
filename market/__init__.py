a49a9c04006cde009f0dc831988d950ff71d9e0f
HatCS <alek5andr.grv@gmail.com>
2022-04-22 21:12:54 -0400

    Logout and Customizations

--- market/__init__.py
+++ market/__init__.py
@@ -11,4 +11,6 @@ app.config['SECRET_KEY'] = '374504f9ce994f11fd8159a0'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import  routes
--- market/routes.py
+++ market/routes.py
@@ -4,13 +4,14 @@ from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market.forms import RegisterForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route("/")
 @app.route('/home')
 def home_page():
     return render_template('Home.html')
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -24,6 +25,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
         return redirect(url_for('market_page'))
     if form.errors != {}: # If there are not errors from the validations
         for err_msg in form.errors.values():
@@ -47,4 +50,10 @@ def login_page():
 
     return render_template('login.html', form=form)
     return render_template('register.html',form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
 
